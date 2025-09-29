d056843e0e0ea94cdfa399a752bab5c30906ada4
noa bouskila <noabouskil@gmail.com>
2025-09-29 11:42:11 +0200

    ♻️ FO1: LOGIN >Create LoginForm

---
+++ src/components/pages/login/LoginPage.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import LoginForm from './LoginForm';
+
+export default function LoginPage() {
+
+ 
+
+
+
+  return (
+    <div>
+      {/* <Logo/> */}
+      <LoginForm />
+    </div>
+  );
+}
