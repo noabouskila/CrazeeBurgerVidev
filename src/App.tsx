e3d19f92fb746faa6ea390155940e00c5e4a5c85
noa bouskila <noabouskil@gmail.com>
2025-09-29 14:36:22 +0200

    🐛 F01 : conflit merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 // import React from "react";
+import { BrowserRouter , Routes ,Route} from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
 
+
+
+
 function App() {
-  
-  return <LoginPage/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+       
+      </Routes>
+    </BrowserRouter>
+  );
   
 }
 
--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -3,10 +3,6 @@ import LoginForm from './LoginForm';
 
 export default function LoginPage() {
 
- 
-
-
-
   return (
     <div>
       {/* <Logo/> */}
