48a109736e76d31800228aeb5e61ee8fea2e3db4
noa bouskila <noabouskil@gmail.com>
2025-10-21 13:05:18 +0200

    ✨ F02  : add redirection deconnexion

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import {  Routes ,Route , Navigate} from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
-import OrderPage from "./components/pages/order/Order-Page";
+import OrderPage from "./components/pages/order/OrderPage";
 import ErrorPage from "./components/pages/error/ErrorPage";
 
 
---
+++ src/components/pages/order/OrderPage.tsx
@@ -0,0 +1,16 @@
+import { Link, useLocation} from "react-router-dom";
+
+
+export default function OrderPage() {
+
+    const location = useLocation();
+    const prenom = location.state?.prenom || "invité";
+ 
+
+  return (
+   <div>
+    <h1>Bienvenue {prenom}</h1>
+    <Link to="/">Deconnexion</Link>
+   </div>
+  )
+}
