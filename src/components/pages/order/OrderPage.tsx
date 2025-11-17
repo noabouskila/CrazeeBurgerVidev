ae0f647290b2906d910cdcbbf6a38985823d1d0b
noa bouskila <noabouskil@gmail.com>
2025-11-17 14:03:10 +0100

    🚧 F04 :  ORDER> Add container

--- src/components/pages/order/OrderPage.tsx
+++ src/components/pages/order/OrderPage.tsx
@@ -1,4 +1,6 @@
 import { Link , useParams} from "react-router-dom";
+import styled from 'styled-components';
+import { theme } from "../../../theme";
 
 
 export default function OrderPage() {
@@ -7,11 +9,34 @@ export default function OrderPage() {
     
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Deconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Deconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyled>
   );
 }
+
+
+
+
+export const OrderPageStyled = styled.section`
+  background-color: ${theme.colors.primary};
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .container {
+    background-color: ${theme.colors.white};
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+ 
