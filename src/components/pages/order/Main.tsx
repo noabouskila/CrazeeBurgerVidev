34dafc9b7216e4888b96d7f36f4d61e34c71439e
noa bouskila <noabouskil@gmail.com>
2025-11-20 11:17:20 +0100

    🚧 F05 : ORDER > MAIN > add picture , fake data , math.js and grid layout

--- src/components/pages/order/Main.tsx
+++ src/components/pages/order/Main.tsx
@@ -1,20 +1,36 @@
 
+import styled from 'styled-components';
+import Basket from './basket';
+import Menu from './Menu';
 
 export default function Main() {
   return (
-   <MainStyled className="main">main</MainStyled>
+   <MainStyled className="main">
+        <Basket/>
+        <Menu/>
+   </MainStyled>
   )
 }
 
 
-import styled from 'styled-components';
 
-export const MainStyled  = styled.section`
- 
-    flex: 1;
-    box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
-    border-bottom-left-radius: 15px;
-    border-bottom-right-radius: 15px;
 
+
+export const MainStyled = styled.section`
+  flex: 1;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  background: #f5f5f7;
+  padding: 50px 50px 150px;
+  grid-row-gap: 60px;
+
+    display: grid;
+    grid-template-columns: 25% 75%;
+  
 `;
 
+
+
+// CSS pour les cartes produit :
+  //  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
---
+++ src/components/pages/order/Menu.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default function Menu() {
+  return (
+    <MenuStyled>
+        Menu
+    </MenuStyled>
+  )
+}
+
+
+export const MenuStyled = styled.section`
+
+background-color: blue;
+  
+`;
+
