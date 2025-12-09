1e64d33730a9b15e05576591cb9bb8d98940909b
noa bouskila <noabouskil@gmail.com>
2025-12-09 13:27:49 +0100

    🚧 F09 : ADMIN> EditProductForm  > display info of cards clicked on to tab editProduct

--- src/components/pages/order/Main/Admin/AdminPanel/EditProductForm.tsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditProductForm.tsx
@@ -2,19 +2,66 @@ import styled from "styled-components";
 import HintMessage from "./HintMessage";
 import { useContext } from "react";
 import OrderContext from "../../../../../../context/OrderContext";
+import ImagePreview from "./ImagePreview";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import { getTextInputConfig } from "./getTextInputConfig";
 
 export default function EditProductForm() {
 
-  const {productSelected}  = useContext(OrderContext)
+  const { productSelected, setProductSelected } = useContext(OrderContext);
+  const TextInputs = getTextInputConfig(productSelected);
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setProductSelected({
+      ...productSelected,
+      [name]: value,
+    });
+  };
+
+  if (!productSelected.id) {
+    return <HintMessage />
+  }
   
   return (
     <EditProductFormStyled>
-      <HintMessage />
-      <span>{productSelected && productSelected.title} </span>
+      <ImagePreview
+        imageSource={productSelected.imageSource}
+        title={productSelected.title}
+      />
+
+      <div className="input-fields">
+        {TextInputs.map((input) => (
+          <TextInput
+            key={input.id}
+            {...input}
+            version="minimalist"
+            onChange={handleChange}
+          />
+        ))}
+      </div>
+
+    
     </EditProductFormStyled>
   );
  
 }
 
 
-export const EditProductFormStyled = styled.form``;
+export const EditProductFormStyled = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  width: 70%;
+  height: 100%;
+
+  .input-fields {
+    grid-area: 1/2/-2/3;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+    height: 120px;
+    row-gap: 10px;
+    margin-left: 15px;
+  }
+`;
--- src/enums/products.ts
+++ src/enums/products.ts
@@ -1,7 +1,7 @@
 import type { ProductForm } from "../types/types";
 
 export const EMPTY_PRODUCT: Readonly<ProductForm> = {
-  id: "0",
+  id: "",
   imageSource: "",
   title: "",
   price: "",
