f65a45bca24f9df38c4a4d62cf3f91fdd57d5a12
noa bouskila <noabouskil@gmail.com>
2026-01-13 12:25:23 +0100

    ✨F11 :  HOOKS > create customHooks useBasket

---
+++ src/hooks/useBasket.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+import { fakeBasket } from "../data/fakeBasket"
+
+export const useBasket = () => { 
+    const [basket , setBasket]  = useState(fakeBasket.LARGE)
+    return { basket  }
+ }
