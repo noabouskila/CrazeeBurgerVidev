b55813501f5061da9cbfc0ebc5f648603e7544d7
noa bouskila <noabouskil@gmail.com>
2026-01-19 16:28:24 +0100

    ✨F11 : BASKET> Add product on the basket with quantity

--- src/hooks/useBasket.tsx
+++ src/hooks/useBasket.tsx
@@ -1,11 +1,27 @@
 import { useState } from "react"
 import { fakeBasket } from "../data/fakeBasket"
 import type { MenuItem } from "../types/types";
+import { deepClone, findInArray } from "../utils/array";
 
 export const useBasket = () => { 
     const [basket, setBasket] = useState(fakeBasket.EMPTY);
 
-    const handleAddToBasket = (producToAdd: MenuItem) => {};
+    const handleAddToBasket = (producToAdd: MenuItem) => {
+
+        const basketCopy = deepClone(basket);
+        const productAlreadyInBasket = findInArray(basketCopy , producToAdd.id);
+
+        if(productAlreadyInBasket){
+            productAlreadyInBasket.quantity = (productAlreadyInBasket.quantity ?? 0) + 1;
+        }else{
+            basketCopy.unshift({
+                ...producToAdd , 
+                quantity : 1 
+            })
+        }
+
+        setBasket(basketCopy);
+    };
 
     return { basket, handleAddToBasket };
 
