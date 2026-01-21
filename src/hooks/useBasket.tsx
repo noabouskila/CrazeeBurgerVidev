fc6388a27adacfcedb7461de3bd85419b427114e
noa bouskila <noabouskil@gmail.com>
2026-01-21 12:51:23 +0100

    ♻️ F11 : BASKET>refacto useBasket

--- src/hooks/useBasket.tsx
+++ src/hooks/useBasket.tsx
@@ -1,28 +1,52 @@
 import { useState } from "react"
 import { fakeBasket } from "../data/fakeBasket"
 import type { MenuItem } from "../types/types";
-import { deepClone, findInArray } from "../utils/array";
+import {  findInArray } from "../utils/array";
 
 export const useBasket = () => { 
+
     const [basket, setBasket] = useState(fakeBasket.EMPTY);
 
-    const handleAddToBasket = (producToAdd: MenuItem) => {
+    const handleAddToBasket = (productToAdd: MenuItem) => {
+        setBasket((prevBasket) => {
+            const productAlreadyInBasket = findInArray(prevBasket, productToAdd.id);
 
-        const basketCopy = deepClone(basket);
-        const productAlreadyInBasket = findInArray(basketCopy , producToAdd.id);
+            if (!productAlreadyInBasket) {
+            return [{ ...productToAdd, quantity: 1 }, ...prevBasket];
+            }
 
-        if(productAlreadyInBasket){
-            productAlreadyInBasket.quantity = (productAlreadyInBasket.quantity ?? 0) + 1;
-        }else{
-            basketCopy.unshift({
-                ...producToAdd , 
-                quantity : 1 
-            })
-        }
-
-        setBasket(basketCopy);
+            return prevBasket.map((item) =>
+            item.id === productToAdd.id
+                ? { ...item, quantity: (item.quantity ?? 0) + 1 }
+                : item
+            );
+        });
     };
+
+
 
     return { basket, handleAddToBasket };
 
 }
+
+//     const handleAddToBasket = (productToAdd: MenuItem) => {
+
+//         const basketCopy = deepClone(basket);
+
+//         const productAlreadyInBasket = findInArray(basketCopy, productToAdd.id);
+
+//         if (!productAlreadyInBasket) {
+//         const newBasketProduct = {
+//             ...productToAdd,
+//             quantity: 1,
+//         };
+
+//         setBasket([newBasketProduct, ...basketCopy]);
+//         return;
+//         }
+
+//         productAlreadyInBasket.quantity =
+//         (productAlreadyInBasket.quantity ?? 0) + 1;
+
+//         setBasket(basketCopy);
+//    };
