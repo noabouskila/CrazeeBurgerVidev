e14b109c58dfb784f191e7771463c36a6596ba2a
noa bouskila <noabouskil@gmail.com>
2026-02-10 17:59:46 +0100

    💫 F14 :BasketProducts >add animation on basket

--- src/components/pages/order/Main/Basket/BasketProducts.tsx
+++ src/components/pages/order/Main/Basket/BasketProducts.tsx
@@ -2,10 +2,10 @@ import styled from "styled-components";
 import BasketCard from "./BasketCard";
 import { DEFAULT_IMAGE } from "../../../../../enums/products";
 import { findObjectById } from "../../../../../utils/array";
-import { useContext } from "react";
+import { createRef, useContext, useRef } from "react";
 import OrderContext from "../../../../../context/OrderContext";
 import { checkIsProductSelected } from "../Menu/helper";
-
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 
 
@@ -21,7 +21,7 @@ export default function BasketProducts() {
     username
   } = useContext(OrderContext);
 
-
+  const nodeRefs = useRef<Record<string, React.RefObject<HTMLDivElement>>>({});
 
   const handleOnDelete = (e: React.MouseEvent<HTMLDivElement> , id: string ,username : string ) => {
     e.stopPropagation()
@@ -30,35 +30,53 @@ export default function BasketProducts() {
 
   return (
     <BasketProductsStyled>
-      {basket.map((basketProduct) => {
-        const menuProduct = findObjectById(menu, basketProduct.id);
-       
-        if (!menuProduct) {
-          // If the menu item can't be found, skip rendering this basket entry
-          return null;
-        }
+      <TransitionGroup>
+        {basket.map((basketProduct) => {
+          const menuProduct = findObjectById(menu, basketProduct.id);
+          if (!menuProduct) return null;
 
-        return (
-          <div className="basket-card" key={basketProduct.id}>
-            <BasketCard
-              id={menuProduct.id}
-              title={menuProduct.title}
-              imageSource={
-                menuProduct.imageSource
-                  ? menuProduct.imageSource
-                  : DEFAULT_IMAGE
-              }
-              price={menuProduct.price}
-              quantity={basketProduct.quantity}
-              onDelete={(e) => handleOnDelete(e, basketProduct.id , username)}
-              isModeAdmin={isModeAdmin}
-              onClick={() => selectProductForEdit(basketProduct.id)}
-              isSelected={checkIsProductSelected(basketProduct.id,productSelected.id
-              )}
-            />
-          </div>
-        );
-      })}
+          if (!nodeRefs.current[basketProduct.id]) {
+            nodeRefs.current[basketProduct.id] =
+              createRef<HTMLDivElement>() as React.RefObject<HTMLDivElement>;
+          }
+
+          const nodeRef = nodeRefs.current[basketProduct.id];
+
+          return (
+            <CSSTransition
+              key={basketProduct.id}
+              timeout={500}
+              classNames={"animation"}
+              nodeRef={nodeRef}
+              appear={true} // animation au montage du composant
+              
+            >
+              <div className="basket-card" ref={nodeRef}>
+                <BasketCard
+                  id={menuProduct.id}
+                  title={menuProduct.title}
+                  imageSource={
+                    menuProduct.imageSource
+                      ? menuProduct.imageSource
+                      : DEFAULT_IMAGE
+                  }
+                  price={menuProduct.price}
+                  quantity={basketProduct.quantity}
+                  onDelete={(e) =>
+                    handleOnDelete(e, basketProduct.id, username)
+                  }
+                  isModeAdmin={isModeAdmin}
+                  onClick={() => selectProductForEdit(basketProduct.id)}
+                  isSelected={checkIsProductSelected(
+                    basketProduct.id,
+                    productSelected.id,
+                  )}
+                />
+              </div>
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </BasketProductsStyled>
   );
 }
@@ -71,5 +89,47 @@ export const BasketProductsStyled = styled.div`
 
   .basket-card {
     margin: 16px 20px;
+    /* transition: all 0.5s ease; */
+  }
+
+  /* Animation CSS classes */
+  /* Apparition au montage */
+  .basket-card.animation-appear {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+
+  .basket-card.animation-appear-active {
+    transform: translateX(0);
+    opacity: 1;
+    transition:
+      0.5s ease,
+      opacity 2s ease;
+  }
+
+  /* Pour les entrées normales */
+  .basket-card.animation-enter {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+
+  .basket-card.animation-enter-active {
+    transform: translateX(0);
+    opacity: 1;
+    transition:
+      0.5s ease,
+      opacity 0.8s ease;
+  }
+
+  /* Sortie */
+  .basket-card.animation-exit {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+  .basket-card.animation-exit-active {
+    opacity: 0;
+    transition: all 0.5s ease;
+    transform: translateX(-100%);
   }
 `;
